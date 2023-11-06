ddd6d3d83c6044b5c30420ec67665a8b81cf93d4
Gustavo Quiroz <gustavoq26@gmail.com>
2023-11-06 15:16:23 -0600

    web card done with responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "./scss/styles.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   const hyperlinks = document?.querySelectorAll(".icon");
+  //   function addAnimationDelay({ node, index }: any) {
+  //     node.classList.add("scale-delay");
+  //     node.style = "--animationDelay:" + index;
+  //     console.log(node + index);
+  //   }
+  //   hyperlinks.forEach(addAnimationDelay);
+  // }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className='section'>
+      <img
+        className='background'
+        src='./src/assets/flat-hand-drawn-psychedelic-groovy-background_23-2148850121.webp'
+        alt=''
+      />
+      <div className='overlay'></div>
+      <div className='whole transition-translate-y'>
+        <div className='container'>
+          <img
+            className='pfp transition-scale-in'
+            src='https://pbs.twimg.com/profile_images/1659370175546765314/NQtKyiWa_400x400.jpg'
+            alt=''
+          />
+          <hr className='separator-1' />
+          <div className='copy'>
+            <h1 className='transition-translate-y transition-opacity-2'>Kinxori</h1>
+            <p className='transition-translate-y transition-opacity-2'>
+              3D artist, Investor and Web Developer in process. <br /> Keep Creating!
+            </p>
+            <div className='icons'>
+              <a className='icon' href='https://www.instagram.com' target='_blank'>
+                <i className='fa fa-instagram' aria-hidden='true'></i>
+              </a>
+              <a className='icon' href='https://www.facebook.com/kinxori' target='_blank'>
+                <i className='fa fa-facebook' aria-hidden='true'></i>
+              </a>
+              <a className='icon' href='https://www.linkedin.com/in/quinchori/' target='_blank'>
+                <i className='fa fa-linkedin' aria-hidden='true'></i>
+              </a>
+              <a className='icon' href='https://www.kinxori.com/' target='_blank'>
+                <i className='fa-solid fa-window-maximize' aria-hidden='true'></i>
+              </a>
+              <a className='icon' href='https://twitter.com/kinxori' target='_blank'>
+                <i className='fa fa-twitter' aria-hidden='true'></i>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
